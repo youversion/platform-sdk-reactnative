ecb096cf22bcb63ee28dca3bad411cf2a87c19c6
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2025-11-12 22:27:13 -0500

    test: create mocks for module `RNYouVersionPlatform`

---
+++ mocks/RNYouVersionPlatform.ts
@@ -0,0 +1,146 @@
+import {
+  BibleReference,
+  BibleVersion,
+  HighlightResponse,
+  LanguageOverview,
+  SignInWithYouVersionPermission,
+  YouVersionLoginResult,
+  YouVersionUserInfo,
+  YouVersionVerseOfTheDay,
+} from "../src";
+
+export function configure(_appKey: string) {}
+export function setApiHost(_apiHost: string) {}
+export function getAccessToken(): string | null {
+  return "existing-access-token";
+}
+
+export function signIn(
+  requiredPermissions: SignInWithYouVersionPermission[] = [],
+  optionalPermissions: SignInWithYouVersionPermission[] = [],
+): Promise<YouVersionLoginResult> {
+  return Promise.resolve({
+    accessToken: "mock-access-token",
+    permissions: [...requiredPermissions, ...optionalPermissions],
+    yvpUserId: "mock-yvp-user-id",
+  });
+}
+
+export function signOut(): Promise<void> {
+  return Promise.resolve();
+}
+
+export function userInfo(_accessToken?: string): Promise<YouVersionUserInfo> {
+  return Promise.resolve({
+    avatarUrl: "https://example.com/avatar.png",
+    firstName: "Mock",
+    lastName: "User",
+    yvpUserId: "mock-yvp-user-id",
+  });
+}
+
+export function verseOfTheDay(
+  dayOfYear: number,
+): Promise<YouVersionVerseOfTheDay> {
+  return Promise.resolve({
+    day: dayOfYear,
+    passageId: "GEN.1.1",
+  });
+}
+
+export function versions(_languageTag?: string): Promise<BibleVersion[]> {
+  const versions: BibleVersion[] = [
+    {
+      id: 1,
+      abbreviation: "KJV",
+      languageTag: "en",
+      bookCodes: ["GEN", "EXO", "LEV"],
+      copyrightLong: "King James Version Copyright",
+      copyrightShort: "KJV Copyright",
+      localizedAbbreviation: "KJV",
+      localizedTitle: "King James Version",
+      textDirection: "ltr",
+      title: "King James Version",
+    },
+  ];
+
+  return Promise.resolve(versions);
+}
+
+export function version(versionId: number): Promise<BibleVersion> {
+  return Promise.resolve({
+    id: versionId,
+    abbreviation: "KJV",
+    languageTag: "en",
+    bookCodes: ["GEN", "EXO", "LEV"],
+    copyrightLong: "King James Version Copyright",
+    copyrightShort: "KJV Copyright",
+    localizedAbbreviation: "KJV",
+    localizedTitle: "King James Version",
+    textDirection: "ltr",
+    title: "King James Version",
+  });
+}
+
+export function chapter(bibleReference: BibleReference): Promise<string> {
+  return Promise.resolve(
+    "In the beginning God created the heaven and the earth.",
+  );
+}
+
+export function languages(_country?: string): Promise<LanguageOverview[]> {
+  const languages: LanguageOverview[] = [
+    {
+      id: "EN",
+      aliases: ["en"],
+      language: "English",
+      displayNames: {
+        en: "English",
+      },
+      scripts: ["Latn"],
+      variants: [],
+      countries: ["US", "GB"],
+      textDirection: "ltr",
+    },
+  ];
+
+  return Promise.resolve(languages);
+}
+
+export function createHighlight(
+  _versionId: number,
+  _passageId: string,
+  _color: string,
+): Promise<boolean> {
+  return Promise.resolve(true);
+}
+
+export function getHighlights(
+  versionId: number,
+  passageId: string,
+): Promise<HighlightResponse[]> {
+  const highlights: HighlightResponse[] = [
+    {
+      passageId,
+      color: "#FFFF00",
+      bibleId: versionId,
+    },
+  ];
+
+  return Promise.resolve(highlights);
+}
+
+export function updateHighlight(
+  _versionId: number,
+  _passageId: string,
+  _color: string,
+): Promise<boolean> {
+  return Promise.resolve(true);
+}
+
+export function deleteHighlight(
+  _versionId: number,
+  _passageId: string,
+): Promise<boolean> {
+  return Promise.resolve(true);
+}
