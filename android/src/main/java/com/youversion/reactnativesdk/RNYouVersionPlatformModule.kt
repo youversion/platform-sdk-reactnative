fe08a28b978531a7ec8eadebed44ad7b35380956
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2025-11-18 05:26:04 -0500

    feat: add support for `configure` in Kotlin

---
+++ android/src/main/java/com/youversion/reactnativesdk/RNYouVersionPlatformModule.kt
@@ -0,0 +1,21 @@
+package com.youversion.reactnativesdk
+
+import com.youversion.platform.core.YouVersionPlatformConfiguration
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
+
+class RNYouVersionPlatformModule : Module() {
+    override fun definition() = ModuleDefinition {
+        Name("RNYouVersionPlatform")
+
+        Function("configure") { appKey: String ->
+            val context = appContext.reactContext
+                ?: throw IllegalStateException("ReactContext is not available yet")
+
+            YouVersionPlatformConfiguration.configure(
+                context = context,
+                appKey = appKey,
+            )
+        }
+    }
+}
