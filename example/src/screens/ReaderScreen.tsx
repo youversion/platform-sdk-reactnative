2e1f6d3c431c1f33276cb5361946a42ce8ea31e2
Jeff Hampton <jhampton@gmail.com>
2025-11-20 17:20:43 -0500

    feat(docs): update example app language for publishing release

--- example/src/screens/ReaderScreen.tsx
+++ example/src/screens/ReaderScreen.tsx
@@ -3,8 +3,8 @@ import { BibleReaderView } from "@youversion/platform-sdk-reactnative";
 export function ReaderScreen() {
   return (
     <BibleReaderView
-      appName="Sample App"
-      signInMessage="Explore the sample app!"
+      appName="Example App"
+      signInMessage="Explore the example app!"
     />
   );
 }
