22e36fd3f1602ab28765fa2e87c0a762f3d2be38
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2026-02-17 21:35:05 -0500

    feat: implement Bible reader view for Android

--- android/src/main/java/com/youversion/reactnativesdk/RNBibleReaderViewModule.kt
+++ android/src/main/java/com/youversion/reactnativesdk/RNBibleReaderViewModule.kt
@@ -1,6 +1,7 @@
 package com.youversion.reactnativesdk
 
 import com.youversion.reactnativesdk.views.YVPBibleReaderView
+import com.youversion.reactnativesdk.views.YVPBibleWidgetView
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
--- android/src/main/java/com/youversion/reactnativesdk/views/YVPBibleReaderView.kt
+++ android/src/main/java/com/youversion/reactnativesdk/views/YVPBibleReaderView.kt
@@ -1,55 +1,68 @@
 package com.youversion.reactnativesdk.views
 
 import android.content.Context
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import com.youversion.platform.core.bibles.domain.BibleReference
+import com.youversion.platform.reader.BibleReader
 import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.views.AutoSizingComposable
 import expo.modules.kotlin.views.ComposeProps
+import expo.modules.kotlin.views.Direction
 import expo.modules.kotlin.views.ExpoComposeView
+import java.util.EnumSet
+
 
 data class BibleReaderViewProps(
-    // Bible reference
     val versionId: MutableState<Int?> = mutableStateOf(null),
     val bookUSFM: MutableState<String?> = mutableStateOf(null),
     val chapter: MutableState<Int?> = mutableStateOf(null),
     val verse: MutableState<Int?> = mutableStateOf(null),
     val verseStart: MutableState<Int?> = mutableStateOf(null),
     val verseEnd: MutableState<Int?> = mutableStateOf(null),
-
     val appName: MutableState<String?> = mutableStateOf(null),
     val signInMessage: MutableState<String?> = mutableStateOf(null),
-    val hasReference: MutableState<Boolean?> = mutableStateOf(false),
+    val hasReference: MutableState<Boolean?> = mutableStateOf(null)
 ) : ComposeProps
 
 class YVPBibleReaderView(context: Context, appContext: AppContext) :
     ExpoComposeView<BibleReaderViewProps>(context, appContext, withHostingView = true) {
-
     override val props = BibleReaderViewProps()
 
     @Composable
     override fun Content(modifier: Modifier) {
-        // TODO: Replace with actual BibleReaderView composable when Kotlin SDK is ready
-        Box(
-            modifier = modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "BibleReaderView placeholder\n" +
-                       "App: ${props.appName.value}\n" +
-                       "Reference: ${props.bookUSFM.value} ${props.chapter.value}",
-                color = Color.Gray
+        BibleReader(
+            appName = props.appName.value!!,
+            appSignInMessage = props.signInMessage.value!!,
+            bibleReference = bibleReference(),
+        )
+    }
+
+    fun bibleReference(): BibleReference? {
+        val start = props.verseStart.value
+        val end = props.verseEnd.value
+
+        if (start != null && end != null) {
+            return BibleReference(
+                versionId = props.versionId.value!!,
+                bookUSFM = props.bookUSFM.value!!,
+                chapter = props.chapter.value!!,
+                verseStart = start,
+                verseEnd = end
             )
         }
+
+        if (props.hasReference.value == true) {
+            return BibleReference(
+                versionId = props.versionId.value!!,
+                bookUSFM = props.bookUSFM.value!!,
+                chapter = props.chapter.value!!,
+                verse = props.verse.value
+            )
+        }
+
+        return null
     }
 }
