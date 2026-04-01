c8b2ef81e70ba52120e696e22804f23bb5bbf80c
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2026-03-31 21:05:09 -0400

    chore: refactor BibleReaderView to be functional component

--- android/src/main/java/com/youversion/reactnativesdk/views/YVPBibleReaderView.kt
+++ android/src/main/java/com/youversion/reactnativesdk/views/YVPBibleReaderView.kt
@@ -1,16 +1,11 @@
 package com.youversion.reactnativesdk.views
 
-import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Modifier
 import com.youversion.platform.core.bibles.domain.BibleReference
 import com.youversion.platform.reader.BibleReader
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ComposableScope
 import expo.modules.kotlin.views.ComposeProps
-import expo.modules.kotlin.views.ExpoComposeView
 
 const val DEFAULT_BEREAN_STANDARD_BIBLE_VERSION = 3034
 
@@ -26,42 +21,37 @@ data class BibleReaderViewProps(
     val hasReference: MutableState<Boolean?> = mutableStateOf(null)
 ) : ComposeProps
 
-class YVPBibleReaderView(context: Context, appContext: AppContext) :
-    ExpoComposeView<BibleReaderViewProps>(context, appContext, withHostingView = true) {
-    override val props = BibleReaderViewProps()
+@Composable
+fun YVPBibleReaderView(props: BibleReaderViewProps) {
+    BibleReader(
+        appName = props.appName.value ?: "",
+        appSignInMessage = props.signInMessage.value ?: "",
+        bibleReference = bibleReference(props),
+    )
+}
 
-    @Composable
-    override fun ComposableScope.Content() {
-        BibleReader(
-            appName = props.appName.value ?: "",
-            appSignInMessage = props.signInMessage.value ?: "",
-            bibleReference = bibleReference(),
+fun bibleReference(props: BibleReaderViewProps): BibleReference? {
+    val start = props.verseStart.value
+    val end = props.verseEnd.value
+
+    if (start != null && end != null) {
+        return BibleReference(
+            versionId = props.versionId.value ?: DEFAULT_BEREAN_STANDARD_BIBLE_VERSION,
+            bookUSFM = props.bookUSFM.value ?: "JHN",
+            chapter = props.chapter.value ?: 1,
+            verseStart = start,
+            verseEnd = end
         )
     }
 
-    fun bibleReference(): BibleReference? {
-        val start = props.verseStart.value
-        val end = props.verseEnd.value
-
-        if (start != null && end != null) {
-            return BibleReference(
-                versionId = props.versionId.value ?: DEFAULT_BEREAN_STANDARD_BIBLE_VERSION,
-                bookUSFM = props.bookUSFM.value ?: "JHN",
-                chapter = props.chapter.value ?: 1,
-                verseStart = start,
-                verseEnd = end
-            )
-        }
-
-        if (props.hasReference.value == true) {
-            return BibleReference(
-                versionId = props.versionId.value ?: DEFAULT_BEREAN_STANDARD_BIBLE_VERSION,
-                bookUSFM = props.bookUSFM.value ?: "JHN",
-                chapter = props.chapter.value ?: 1,
-                verse = props.verse.value
-            )
-        }
-
-        return null
+    if (props.hasReference.value == true) {
+        return BibleReference(
+            versionId = props.versionId.value ?: DEFAULT_BEREAN_STANDARD_BIBLE_VERSION,
+            bookUSFM = props.bookUSFM.value ?: "JHN",
+            chapter = props.chapter.value ?: 1,
+            verse = props.verse.value
+        )
     }
+
+    return null
 }
