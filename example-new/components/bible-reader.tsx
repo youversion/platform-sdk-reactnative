d5356122210a8c156bf4ecd7d6e5d4182787a522
Cameron Pak <cameronandrewpak@gmail.com>
2026-04-13 16:46:38 -0500

    Remove unused ParallaxScrollView
    The `ParallaxScrollView` component and its related imports and styling
    were removed from the home screen, as they were no longer necessary. The
    `BibleReader` component is now directly rendered within a `ThemedView`.

--- example-new/components/bible-reader.tsx
+++ example-new/components/bible-reader.tsx
@@ -12,8 +12,8 @@ export default function App() {
   return (
     <YouVersionProvider appKey="E3hCj5PiDJfMZN6pCBoN2MGRnBQYXWjD2SMDbZA9XjFCCnLg">
       <BibleReader.Root defaultVersionId={3034}>
-        <BibleReader.Toolbar />
         <BibleReader.Content />
+        {/*<BibleReader.Toolbar />*/}
       </BibleReader.Root>
     </YouVersionProvider>
   )
