e72049085bf23da0f32fdbe9c1d23e8a1a204375
Cameron Pak <cameronandrewpak@gmail.com>
2026-04-13 16:43:06 -0500

    Refactor bible reader to use BibleReader component
    Replace the deprecated `BibleTextView` component with the newer
    `BibleReader` component, which offers a more structured and extensible
    approach to displaying Bible content. This change also introduces the
    `BibleReader.Toolbar` and `BibleReader.Content` components for better
    modularity.

--- example-new/components/bible-reader.tsx
+++ example-new/components/bible-reader.tsx
@@ -1,6 +1,6 @@
 'use dom'
 
-import { YouVersionProvider, BibleTextView } from '@youversion/platform-react-ui'
+import { YouVersionProvider, BibleReader } from '@youversion/platform-react-ui'
 import * as Crypto from 'expo-crypto'
 
 type UUID = `${string}-${string}-${string}-${string}-${string}`
@@ -11,7 +11,10 @@ window.crypto.randomUUID = () => Crypto.randomUUID() as UUID
 export default function App() {
   return (
     <YouVersionProvider appKey="E3hCj5PiDJfMZN6pCBoN2MGRnBQYXWjD2SMDbZA9XjFCCnLg">
-      <BibleTextView reference="JHN.1.1-4" versionId={3034} />
+      <BibleReader.Root defaultVersionId={3034}>
+        <BibleReader.Toolbar />
+        <BibleReader.Content />
+      </BibleReader.Root>
     </YouVersionProvider>
   )
 }
