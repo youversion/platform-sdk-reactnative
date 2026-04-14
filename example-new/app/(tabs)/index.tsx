004975fd84187170ae4ad80b55977a3c02428513
Cameron Pak <cameronandrewpak@gmail.com>
2026-04-14 09:31:03 -0500

    Remove unused styles from HomeScreen
    The `styles.stepContainer` and its properties were not being utilized
    and have been removed to simplify the component's styling. The `flex: 1`
    style has been directly applied to `ThemedView` to ensure it occupies
    available space.

--- example-new/app/(tabs)/index.tsx
+++ example-new/app/(tabs)/index.tsx
@@ -1,26 +1,10 @@
-import { StyleSheet } from 'react-native'
-
-// import { HelloWave } from '@/components/hello-wave'
-// import { ThemedText } from '@/components/themed-text'
 import { ThemedView } from '@/components/themed-view'
-// import { Link } from 'expo-router'
 import BibleReader from '@/components/bible-reader'
 
 export default function HomeScreen() {
   return (
-    <ThemedView style={styles.stepContainer}>
+    <ThemedView style={{ flex: 1 }}>
       <BibleReader dom={{ matchContents: true }} />
     </ThemedView>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  stepContainer: {
-    flex: 1,
-    // gap: 8,
-    // marginBottom: 8,
-  },
-})
