47a30d52c83897d7d3d58a7288415f17162ec193
Cameron Pak <cameronandrewpak@gmail.com>
2026-04-14 09:31:09 -0500

    Add platform check for window.crypto.randomUUID

--- example-new/components/bible-reader.tsx
+++ example-new/components/bible-reader.tsx
@@ -1,19 +1,21 @@
 'use dom'
 
+import { Platform } from 'react-native'
 import { YouVersionProvider, BibleReader } from '@youversion/platform-react-ui'
 import * as Crypto from 'expo-crypto'
 
 type UUID = `${string}-${string}-${string}-${string}-${string}`
 
 // We need to monkey patch this since the window object doesn't exist, but the YouVersionProvider relies on it.
-window.crypto.randomUUID = () => Crypto.randomUUID() as UUID
+if (Platform.OS !== 'web') {
+  window.crypto.randomUUID = () => Crypto.randomUUID() as UUID
+}
 
 export default function App() {
   return (
     <YouVersionProvider appKey="E3hCj5PiDJfMZN6pCBoN2MGRnBQYXWjD2SMDbZA9XjFCCnLg">
       <BibleReader.Root defaultVersionId={3034}>
         <BibleReader.Content />
-        {/*<BibleReader.Toolbar />*/}
       </BibleReader.Root>
     </YouVersionProvider>
   )
